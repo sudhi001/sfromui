764ea87ac835a9f8cb999ef8d3b2df0b6726b2f3
Sudhi S <sudhis@live.com>
2024-05-25 21:10:10 +0530

    fixes

--- src/components/Form/InputField.js
+++ src/components/Form/InputField.js
@@ -23,7 +23,7 @@ const InputField = ({ formData, field, value, error, handleChange }) => {
         />
       ) : field.select ? (
         <select
-          className="max-w-sm w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 "
+          className="block max-w-sm w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 rounded-lg "
           value={value}
           onChange={handleInputChange}
         >
